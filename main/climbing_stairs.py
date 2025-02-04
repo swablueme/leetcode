f80f1ca4dbed7b530d2d68adc7f3747f713b1293
swablueme <swablueme@gmail.com>
2025-02-04 20:01:32 +1100

    improve recursion

--- main/climbing_stairs.py
+++ main/climbing_stairs.py
@@ -4,14 +4,14 @@ import functools
 def climbStairs(n: int) -> int:
 
     @functools.lru_cache(maxsize=100, typed=False)
-    def climb(n, lst):
+    def climb(n):
         if n == 0:
             return 1
         elif n < 0:
             return 0
         else:
-            return climb(n - 1, lst + (1,)) + climb(n - 2, lst + (2,))
-    return climb(n, ())
+            return climb(n - 1) + climb(n - 2)
+    return climb(n)
 
 
 print(climbStairs(30))
