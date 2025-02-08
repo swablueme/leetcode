5e56c0cb17b4052997babe991ded9f854c3255aa
swablueme <swablueme@gmail.com>
2025-02-08 19:10:37 +1100

    add O(MN) complexity

--- main/group_anagrams.py
+++ main/group_anagrams.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 
 
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    # O(M x NlogN) complexity
     d = defaultdict(list)
 
     value = groupby(strs, key=lambda x: sorted(x))
@@ -12,6 +13,23 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
     return list(d.values())
 
 
-print(groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"]))
-print(groupAnagrams([""]))
-print(groupAnagrams(["a"]))
+def translate_word(word):
+    word_array = [0] * 26
+    starting_index_characters = ord('a')
+    for character in word:
+        index = ord(character) - starting_index_characters
+        word_array[index] += 1
+    return tuple(word_array)
+
+
+def groupAnagrams2(strs: List[str]) -> List[List[str]]:
+    # O(MN) complexity
+    d = defaultdict(list)
+    [d[translate_word(word)].append(word) for word in strs]
+    print(d)
+    return d.values()
+
+
+print(groupAnagrams2(["act", "pots", "tops", "cat", "stop", "hat"]))
+print(groupAnagrams2([""]))
+print(groupAnagrams2(["a"]))
