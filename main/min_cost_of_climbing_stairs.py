27d569b113aaa410189545d91a381af28673cff7
swablueme <swablueme@gmail.com>
2025-02-17 18:41:56 +1100

    use mins

--- main/min_cost_of_climbing_stairs.py
+++ main/min_cost_of_climbing_stairs.py
@@ -1,39 +1,44 @@
 from typing import List
 import functools
+import math
 
 
 def minCostClimbingStairs(cost):
-    foundTotalCosts = []
-
-    paths = {}
+    total_cost = math.inf
 
     @functools.lru_cache()
-    def climb(cost: List[int], i=0, totalCost=0, path=()) -> int:
+    def climb(cost: List[int], i=0, found_total_cost=0) -> int:
+        nonlocal total_cost
         if i > len(cost) - 1:
-            if path not in paths:
-                paths[path] = totalCost
-            else:
-                print(path, "already taken!")
-            foundTotalCosts.append(totalCost)
+            total_cost = min(total_cost, found_total_cost)
             return
 
         if i == 0:
             j = 1
             foundCost = cost[j]
             climb(
-                cost, j + 1, totalCost + foundCost, path + (f"{j} => {j+1}", ))
+                cost, j + 1, found_total_cost + foundCost)
             climb(
-                cost, j + 2, totalCost + foundCost, path + (f"{j} => {j+2}",))
+                cost, j + 2, found_total_cost + foundCost)
         foundCost = cost[i]
         climb(
-            cost, i + 1, totalCost + foundCost, path + (f"{i} => {i+1}", ))
+            cost, i + 1, found_total_cost + foundCost)
         climb(
-            cost, i + 2, totalCost + foundCost, path + (f"{i} => {i+2}", ))
+            cost, i + 2, found_total_cost + foundCost)
     climb(tuple(cost))
-    print(foundTotalCosts)
-    return min(foundTotalCosts)
+
+    return total_cost
 
 
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        return minCostClimbingStairs(cost)
+
+
+assert minCostClimbingStairs([100, 99]) == 99
 assert minCostClimbingStairs([1, 2]) == 1
 assert minCostClimbingStairs([1, 2, 3]) == 2
 assert minCostClimbingStairs([1, 2, 1, 2, 1, 1, 1]) == 4
+assert minCostClimbingStairs([10, 15, 20]) == 15
+assert minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
+# Solution().minCostClimbingStairs([100, 99])
